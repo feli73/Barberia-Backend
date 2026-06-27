df39896e5990dd3b487fdef94ece0e710abc2d6d
feli73 <126608472+feli73@users.noreply.github.com>
2026-06-27 12:14:15 -0300

    comit dieciseis commit

--- src/services/mailingService.js
+++ src/services/mailingService.js
@@ -5,14 +5,18 @@ import dns from "dns";
 dotenv.config();
 
 
+dns.lookup("smtp.gmail.com", { all: true }, (err, addresses) => {
+  console.log("SMTP DNS:", err, addresses);
+});
+
+
 // 🔥 FIX IMPORTANTE para IPv6 en Render
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-   host: "smtp.gmail.com",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
-  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS
