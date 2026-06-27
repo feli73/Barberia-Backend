2390eba035f4586bbd22613b44d72c974cb0b4f5
feli73 <126608472+feli73@users.noreply.github.com>
2026-06-27 12:07:59 -0300

    commit quince

--- src/services/mailingService.js
+++ src/services/mailingService.js
@@ -9,7 +9,10 @@ dotenv.config();
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
- service: "gmail",
+   host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS
